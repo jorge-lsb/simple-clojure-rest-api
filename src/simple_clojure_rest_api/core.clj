d362972006b412dec73dfe09809419e7d8c0ec9e
jorge-lsb <jorge.barbosa@nubank.com.br>
2025-03-16 22:48:43 -0300

    modify core-file

--- src/simple_clojure_rest_api/core.clj
+++ src/simple_clojure_rest_api/core.clj
@@ -1,7 +1,21 @@
 (ns simple-clojure-rest-api.core
-  (:gen-class))
+  (:gen-class)
+  (:require [simple-clojure-rest-api.diplomat.http-client :as http-client]))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println "---------------------------------")
+  (println "Select one option below")
+  (println "[1] get books")
+  (println "[2] get movies")
+  (println "[3] get character-elf")
+  (println "[4] get character-human")
+  (println "[5] get all character races")
+  (println "---------------------------------")
+  (let [input-string (read-line)]
+    (case input-string
+      "1" (println (http-client/get-books))
+      "2" (println (http-client/get-movies))
+      "3" (println (http-client/get-characters :elf))
+      "4" (println (http-client/get-characters :human))
+      "5" (println (http-client/get-characters nil)))))
