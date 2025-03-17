495b71afddb45d6a9366e48a5ca9f5cdb321a37d
Jorge Barbosa <61752234+jorge-lsb@users.noreply.github.com>
2025-03-16 21:49:37 -0300

    create a project template

---
+++ test/simple_clojure_rest_api/core_test.clj
@@ -0,0 +1,7 @@
+(ns simple-clojure-rest-api.core-test
+  (:require [clojure.test :refer :all]
+            [simple-clojure-rest-api.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
