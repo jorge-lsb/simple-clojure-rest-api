9e5b974b605feebba9d7ad63c1df2a5212267164
jorge-lsb <jorge.barbosa@nubank.com.br>
2025-03-16 22:41:06 -0300

    add http-client-file

---
+++ src/simple_clojure_rest_api/diplomat/http_client.clj
@@ -0,0 +1,39 @@
+(ns simple-clojure-rest-api.diplomat.http-client
+  (:require [cheshire.core :as json]
+            [clj-http.client :as client]
+            [rest-api-tutorial.config.auth-config :as auth-config]
+            [rest-api-tutorial.schema.book :as schema.book]
+            [rest-api-tutorial.schema.movie :as schema.movie]
+            [rest-api-tutorial.adapters.book :as adapters.book]
+            [rest-api-tutorial.adapters.movie :as adapters.movie]
+            [rest-api-tutorial.adapters.character-profile :as adapters.character-profile]
+            [rest-api-tutorial.schema.character-profile :as schema.character-profile]
+            [schema.core :as s]))
+
+(def bookmark-settings
+  {:book-url       "https://the-one-api.dev/v2/book"
+   :movie-url      "https://the-one-api.dev/v2/movie"
+   :characters-url "https://the-one-api.dev/v2/character?limit=50"})
+
+(s/defn get-characters :- schema.character-profile/CharactersResponse
+  [race :- (s/maybe schema.character-profile/Race)]
+  (let [response (client/get (:characters-url bookmark-settings) auth-config/header-map)
+        body-parsed (json/parse-string (:body response) true)
+        characters (->> (:docs body-parsed) (map #(adapters.character-profile/response->character %)))]
+    (if race
+      {:characters (filter #(= (:race %) race) characters)}
+      {:characters characters})))
+
+(s/defn get-movies :- schema.movie/MoviesResponse
+  []
+  (let [response (client/get (:movie-url bookmark-settings) auth-config/header-map)
+        body-parsed (json/parse-string (:body response) true)
+        movies (->> (:docs body-parsed) (map #(adapters.movie/response->movies %)))]
+    {:movies movies}))
+
+(s/defn get-books :- schema.book/BooksResponse
+  []
+  (let [response (client/get (:book-url bookmark-settings))
+        body-parsed (json/parse-string (:body response) true)
+        books (->> (:docs body-parsed) (map #(adapters.book/response->books %)))]
+    {:books books}))
